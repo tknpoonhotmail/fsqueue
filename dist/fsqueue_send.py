76764b10885965ba1f28d24a497ebf408c11d998
Tony Poon <tknpoon@hotmail.com>
2022-01-01 19:08:57 +0800

    .

--- dist/fsqueue_send.py
+++ dist/fsqueue_send.py
@@ -2,11 +2,19 @@
 import os,sys
 #######################################
 sys.dont_write_bytecode = True
-scriptbindir=os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(scriptbindir,'..','lib'))
+# scriptbindir=os.path.dirname(os.path.realpath(__file__))
+# sys.path.append(os.path.join(scriptbindir))
 #######################################
 
-from fsqueue import FsQueue
+from FsQueue import FsQueue
 
 testq = FsQueue("testq",10)
+ba = bytearray("a_string".encode())
+msg =[]
+msg.append({"key":True, "blobs":{"a.bin":ba ,"b.bin":ba} })
+msg.append({"key":False})
+msg.append({"key":True, "blobs":{"b.bin":ba } })
+
+testq.send(msg)
+
 
